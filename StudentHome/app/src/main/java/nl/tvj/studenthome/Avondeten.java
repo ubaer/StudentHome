5bc573ba6dbf068f566280d19a42f6c7e12d6009
nino-vrijman <ninovrijman@gmail.com>
2015-10-31 11:55:03 +0100

    Paradigm project incl. flowchart

--- StudentHome/app/src/main/java/nl/tvj/studenthome/Avondeten.java
+++ StudentHome/app/src/main/java/nl/tvj/studenthome/Avondeten.java
@@ -7,13 +7,6 @@ import java.util.Date;
  * Created by Kevin on 15-10-2015.
  */
 public class Avondeten extends Activiteit {
-
-    double totaalbedrag;
-    String omschrijving;
-    Gebruiker host;
-    ArrayList<Gebruiker> deelnemers;
-    ArrayList<Beoordeling>beoordelingen;
-    Date startijd;
     ArrayList<String>votingOptions;
 
     Avondeten(int id, double totaalbedrag, String omschrijving, Gebruiker host, Date startijd) {
